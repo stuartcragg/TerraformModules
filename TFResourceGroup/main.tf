708364f1375c3621863a37e52076bae7df13c28c
Stuart Cragg <stuart.cragg@capgemini.com>
2025-01-29 15:51:14 +0100

    added outputs to rg module

--- TFResourceGroup/main.tf
+++ TFResourceGroup/main.tf
@@ -28,3 +28,15 @@ resource "azurerm_resource_group" "rg" {
 
   tags = var.tags
 }
+
+//Create the outputs
+
+output "resource_group_name" {
+    description = "The name of the resource group"
+    value = azurerm_resource_group.rg.name
+}
+
+output "resource_group_id" {
+    description = "ID of the resource group"
+    value = azurerm_resource_group.rg.id
+}
