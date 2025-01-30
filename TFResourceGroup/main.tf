f01ba5125adc44d84e358083e3e0b4ce6478e55c
Stuart Cragg <stuart.cragg@capgemini.com>
2025-01-30 10:16:35 +0100

    Updated mi name output

--- TFResourceGroup/main.tf
+++ TFResourceGroup/main.tf
@@ -36,7 +36,7 @@ output "rg_name" {
     value = azurerm_resource_group.rg.name
 }
 
-output "resource_group_id" {
+output "rg_id" {
     description = "ID of the resource group"
     value = azurerm_resource_group.rg.id
 }
